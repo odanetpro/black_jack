c33f73a8471e31add9608921fde227ce075556ae
oda <olen2000@yandex.ru>
2022-04-09 20:37:44 +0900

    modify main file

--- game.rb
+++ game.rb
@@ -4,42 +4,124 @@ require './card'
 require './deck'
 require './player'
 
-def new_game
+def start_new_game
+  @game_con = 0
   @deck.new_deck
-  @player.new_game
-  @dealer.new_game
+
+  @players.each_value do |player|
+    player.new_game
+    dealing player
+    beting player, GAME_BET
+  end
 end
 
 def dealing(player)
-  player.add_card(@deck.give_card)
-  player.add_card(@deck.give_card)
+  2.times { player.add_card(@deck.give_card) }
 end
 
-def beting(player)
-  player.make_bet(10)
-rescue e
+def beting(player, bet)
+  player.make_bet(bet)
+  @game_con += bet
+rescue RuntimeError => e
   puts e.message.to_s
   exit
 end
 
-def display_field
-  # очистка экрана
-  puts "\e[H\e[2J"
-
-  # puts "Вы: #{@player.cards} Очки: #{@player.count_points}"
+def display_game_field
+  @players.each_value do |player|
+    player.dealer? ? show_hidden_player_info(player) : show_player_info(player)
+  end
 end
 
+def show_player_info(player)
+  puts "#{player.name}: #{player_cards(player)} Очки: #{player.count_points} "
+end
+
+def player_cards(player)
+  player.cards.map(&:name).join(' ')
+end
+
+def show_hidden_player_info(player)
+  print "#{player.name}: "
+  player.cards.each { print '* ' }
+end
+
+def human_turn(player)
+  print "\n\n1 - пропустить, 2 - взять карту, 3 - открыть карты? "
+  input = gets.to_i
+  puts
+
+  case input
+  when 2
+    player.add_card @deck.give_card
+  when 3
+    open_cards
+  end
+end
+
+def computer_turn(player)
+  if player.count_points < 17
+    player.add_card @deck.give_card
+    puts "дилер берет карту...\n\n"
+  else
+    puts "дилер пропускает ход...\n\n"
+  end
+end
+
+def open_cards
+  @players.each_value { |player| show_player_info player }
+  results
+end
+
+def results
+  who_wins = winner
+  if who_wins
+    puts "Победил #{who_wins.name}"
+  else
+    puts 'Ничья'
+  end
+  exit
+end
+
+def winner
+  players = @players.values.select { |player| player.count_points <= 21 }
+
+  case players.size
+  when 1
+    players[0]
+  when 2
+    player_with_most_points(players)
+  end
+end
+
+def player_with_most_points(players)
+  return if players[0].count_points == players[1].count_points
+
+  players[0].count_points > players[1].count_points ? players[0] : players[1]
+end
+
+def three_cards?
+  @players.each_value { |player| return true if player.cards.size == 3 }
+  false
+end
+
+GAME_BET = 10
+START_BANK = 100
+
 @deck = Deck.new
-@gamer = Player.new
-@dealer = Player.new
-@game_con = 0
+@players = {}
 
-dealing(@gamer)
-beting(@gamer)
+# puts "Добро пожаловать в казино! Как вас зовут?"
+# @user = Player.new(gets.strip.capitalize, START_BANK)
+@players[:user] = Player.new('Игрок', START_BANK, :user)
+@players[:dealer] = Player.new('Дилер', START_BANK, :dealer)
 
-dealing(@dealer)
-beting(@dealer)
+start_new_game
 
-# display_field
-p @gamer.count_points
-p @dealer.count_points
+loop do
+  open_cards if three_cards?
+
+  display_game_field
+  human_turn @players[:user]
+  computer_turn @players[:dealer]
+end
