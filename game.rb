8c64332d2d26f05bcdf75637fe86512298b81e85
oda <olen2000@yandex.ru>
2022-04-10 20:11:55 +0900

    finish main file

--- game.rb
+++ game.rb
@@ -10,8 +10,8 @@ def start_new_game
 
   @players.each_value do |player|
     player.new_game
-    dealing player
-    beting player, GAME_BET
+    dealing(player)
+    beting(player, GAME_BET)
   end
 end
 
@@ -28,6 +28,7 @@ rescue RuntimeError => e
 end
 
 def display_game_field
+  puts
   @players.each_value do |player|
     player.dealer? ? show_hidden_player_info(player) : show_player_info(player)
   end
@@ -46,41 +47,46 @@ def show_hidden_player_info(player)
   player.cards.each { print '* ' }
 end
 
-def human_turn(player)
-  print "\n\n1 - пропустить, 2 - взять карту, 3 - открыть карты? "
-  input = gets.to_i
-  puts
+def show_players_bank
+  print 'В банке у игроков: '
+  @players.each_value { |player| print "#{player.name} - #{player.bank} " }
+end
 
-  case input
-  when 2
-    player.add_card @deck.give_card
-  when 3
-    open_cards
+def human_choice
+  loop do
+    print "\n\n1 - пропустить, 2 - взять карту, 3 - открыть карты? "
+    choice = gets.to_i
+
+    return choice if (1..3).include?(choice)
+
+    print "\nнеизвестное значение, повторите ввод..."
   end
 end
 
 def computer_turn(player)
   if player.count_points < 17
-    player.add_card @deck.give_card
-    puts "дилер берет карту...\n\n"
+    player.add_card(@deck.give_card)
+    puts "\nдилер берет карту..."
   else
-    puts "дилер пропускает ход...\n\n"
+    puts "\nдилер пропускает ход..."
   end
 end
 
 def open_cards
-  @players.each_value { |player| show_player_info player }
-  results
+  puts
+  @players.each_value { |player| show_player_info(player) }
 end
 
-def results
+def summing_up
   who_wins = winner
+
   if who_wins
-    puts "Победил #{who_wins.name}"
+    puts "\nПобедил #{who_wins.name}\n\n"
+    who_wins.get_win(@game_con)
   else
-    puts 'Ничья'
+    puts "\nНичья\n\n"
+    @players.each_value { |player| player.get_win(@game_con / 2) }
   end
-  exit
 end
 
 def winner
@@ -105,23 +111,50 @@ def three_cards?
   false
 end
 
+def play_again?
+  loop do
+    print "\n\nСыграем еще раз? (д/н) "
+    choice = gets.strip.downcase
+
+    return choice == 'д' if %w[д н].include?(choice)
+
+    print "\nнеизвестное значение, повторите ввод..."
+  end
+end
+
 GAME_BET = 10
 START_BANK = 100
 
 @deck = Deck.new
 @players = {}
 
-# puts "Добро пожаловать в казино! Как вас зовут?"
-# @user = Player.new(gets.strip.capitalize, START_BANK)
-@players[:user] = Player.new('Игрок', START_BANK, :user)
+puts 'Добро пожаловать в казино! Как вас зовут?'
+@players[:user] = Player.new(gets.strip.capitalize, START_BANK, :user)
 @players[:dealer] = Player.new('Дилер', START_BANK, :dealer)
 
-start_new_game
-
 loop do
-  open_cards if three_cards?
+  start_new_game
 
-  display_game_field
-  human_turn @players[:user]
-  computer_turn @players[:dealer]
+  loop do
+    display_game_field
+
+    # ход игрока
+    case human_choice
+    when 2
+      @players[:user].add_card(@deck.give_card)
+    when 3
+      break
+    end
+
+    # ход компьютера
+    computer_turn(@players[:dealer])
+
+    break if three_cards?
+  end
+
+  open_cards
+  summing_up
+  show_players_bank
+
+  exit unless play_again?
 end
